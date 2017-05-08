e273579276d8c4f4892db61a0e5b916e131ae25c
violet-anima <violet_anima@zoho.com>
2017-05-08 21:58:32 +0000

    created download_avatars.js file

---
+++ download_avatars.js
@@ -0,0 +1,26 @@
+
+/*
+Using the request package, you will fetch the list of contributors as a JSON string from the GitHub API's
+contributors endpoint. Upon receiving the results, your function will invoke a callback function with
+the results. This callback function will loop over and print out the avatar_url for each object in the
+collection.
+
+*/
+
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!!');
+
+/*
+function getRepoContributors(repoOwner, repoName, cb) {
+  // This function will use the request library to programmatically fetch the list of
+  // contributors via HTTPS for the given repo.
+}
+*/
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
